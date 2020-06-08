2d6cbac0433812c86845c4a9c0b579681b6ac0b9
Romke Reehoorn <romkereehoorn@hotmail.com>
2020-06-08 10:29:05 +0200

    jojo

--- Onderwegthema/js/script.js
+++ Onderwegthema/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
 
-})
+    function calcscore() {
+      var score = 0;
+    
+      $(".acf-radio-list:checked").each(function() {
+        score += parseInt($(this).val(), );
+        console.log(score)
+      });
+      $("input[name=sum]").val(score)
+    }
+    $().ready(function() {
+      $(".acf-radio-list").change(function() {
+        calcscore()
+        
+      });
+    });
+
+    
+    
+    })
